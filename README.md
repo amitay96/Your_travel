d45e72270d63aab0751a031abccbfca40731110d
amitay96 <amitay96@gmail.com>
2022-04-02 23:13:29 +0300

    project done

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
 # Project 4: Around The U.S.
 
-### Overview
+### Overview  
+* Intro  
+* Figma  
+* Adaptive layout
+* GitHub Pages
+* JavaScript
+* Interactive buttons
+  
+**Intro**    
+  
+This is a project that represents a profile of one traveler and the places he went with pictures and option to like.
+  
+## Project features
 
-* Figma
-* Images
+- Semantic HTML5
+- Flexbox
+- Positioning
+- BEM Methodology
+- A custom form
+- CSS Adaptive layout
+- JavaScript application
 
-**Figma**
-
-* [Link to the project in Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
-
-**Images**
-
-The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster. 
-
-Have fun with JavaScript!
+**Figma**  
+  
+* [Link to the project on Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile)  
+  
+**GitHub Pages**  
+  
+* [Link to the project on GitHub Pages](https://amitay96.github.io/web_project_4/)  
+  
---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+const profile = document.querySelector(".profile");
+const form = document.querySelector(".form");
+const popup = document.querySelector(".popup");
+const places = document.querySelector(".places");
+
+const editProfileButton = profile.querySelector(".profile__edit-button");
+const profileName = profile.querySelector(".profile__name");
+const profileTitle = profile.querySelector(".profile__title");
+
+const inputName = form.querySelector(".form__input[name='name']");
+const inputTitle = form.querySelector(".form__input[name='title']");
+const saveButton = form.querySelector(".form__button");
+
+const closeButton = popup.querySelector(".popup__close-button");
+
+const likeButtons = places.querySelectorAll(".place__like-button");
+
+function openForm() {
+  inputName.value = profileName.textContent;
+  inputTitle.value = profileTitle.textContent;
+  popup.classList.add("popup__active");
+}
+
+function closeForm() {
+  popup.classList.remove("popup__active");
+}
+
+function formHandler(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileTitle.textContent = inputTitle.value;
+  closeForm();
+}
+
+editProfileButton.addEventListener("click", openForm);
+closeButton.addEventListener("click", closeForm);
+form.addEventListener("submit", formHandler);
